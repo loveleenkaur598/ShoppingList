030594bb61e18d514f868aa933fab1902d2f4b92
Loveleen <loveleenkaur598@yahoo.com>
2019-10-16 20:46:32 -0400

    Shopping List

--- ShoppingList/ViewController.swift
+++ ShoppingList/ViewController.swift
@@ -18,3 +18,36 @@ class ViewController: UIViewController {
 
 }
 
+// for button borderwidth , radius and bordercolor
+@IBDesignable extension UIButton {
+    
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
+
